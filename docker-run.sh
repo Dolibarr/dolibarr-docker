a89cc047f0ca7547d4bc89336b04d9c2b38c1cff
Laurent Destailleur <eldy@destailleur.fr>
2024-08-21 14:20:52 +0200

    Fix log

--- docker-run.sh
+++ docker-run.sh
@@ -199,7 +199,7 @@ function initializeDatabase()
   if [[ ${DOLI_INIT_DEMO} -eq 1 ]]; then
     mkdir -p /var/www/dev/initdemo/
     versiondemo=`echo "${DOLI_VERSION}" | sed "s/^\([0-9]*\.[0-9]*\).*/\1.0/"`
-    echo "Load demo data from file https://raw.githubusercontent.com/Dolibarr/dolibarr/${DOLI_INIT_DEMO}/dev/initdemo/mysqldump_dolibarr_$versiondemo.sql ..."
+    echo "Load demo data from file https://raw.githubusercontent.com/Dolibarr/dolibarr/${DOLI_VERSION}/dev/initdemo/mysqldump_dolibarr_$versiondemo.sql ..."
     curl -fLSs -o /var/www/dev/initdemo/initdemo.sql https://raw.githubusercontent.com/Dolibarr/dolibarr/${DOLI_VERSION}/dev/initdemo/mysqldump_dolibarr_$versiondemo.sql
     for fileSQL in /var/www/dev/initdemo/*.sql; do
     	# We exclude the old load file.
