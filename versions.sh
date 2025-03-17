386efb105911061f715b8e02d9f21365c9180efd
Jon Bendtsen <xcodeauthor@jonb.dk>
2025-03-17 21:27:42 +0100

    adding version 21 to build list

--- versions.sh
+++ versions.sh
@@ -3,7 +3,7 @@
 set -e
 
 # The list of version to build docker packages for
-DOLIBARR_VERSIONS=("21.0.0")
+DOLIBARR_VERSIONS=("15.0.3" "16.0.5" "17.0.4" "18.0.6" "19.0.4" "20.0.4" "develop" "21.0.0")
 
 # The version to use when installing dolibarr/dolibarr:latest
 DOLIBARR_LATEST_TAG="21.0.0"
