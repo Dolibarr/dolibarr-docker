8e36c1775dca543e2b4e2ba44f6e27e297005130
Aloïs Micard <a.micard@vold.lu>
2025-10-06 13:45:47 +0200

    Release 22.0.2

--- versions.sh
+++ versions.sh
@@ -3,10 +3,10 @@
 set -e
 
 # The list of version to build docker packages for
-DOLIBARR_VERSIONS=("15.0.3" "16.0.5" "17.0.4" "18.0.8" "19.0.4" "20.0.4" "21.0.4" "22.0.1" "develop")
+DOLIBARR_VERSIONS=("15.0.3" "16.0.5" "17.0.4" "18.0.8" "19.0.4" "20.0.4" "21.0.4" "22.0.2" "develop")
 
 # The version to use when installing dolibarr/dolibarr:latest
-DOLIBARR_LATEST_TAG="22.0.1"
+DOLIBARR_LATEST_TAG="22.0.2"
 
 # The version to use to find the dump file for the init of demo with branch "develop"
 DOLIBARR_VERSION_FOR_INIT_DEMO="22.0"
