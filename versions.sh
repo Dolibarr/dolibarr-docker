5a998faaf71b4ed0968644c7a7f7923dd15c44af
Jon Bendtsen <jonbendtsen@jonb.dk>
2026-05-04 15:43:52 +0200

    remove v15 again

--- versions.sh
+++ versions.sh
@@ -3,7 +3,7 @@
 set -e
 
 # The list of version to build docker packages for
-DOLIBARR_VERSIONS=("15.0.3" "16.0.5" "17.0.4" "18.0.9" "19.0.4" "20.0.4" "21.0.4" "22.0.4" "23.0.2" "develop")
+DOLIBARR_VERSIONS=("16.0.5" "17.0.4" "18.0.9" "19.0.4" "20.0.4" "21.0.4" "22.0.4" "23.0.2" "develop")
 
 # The version to use when installing dolibarr/dolibarr:latest
 DOLIBARR_LATEST_TAG="23.0.2"
